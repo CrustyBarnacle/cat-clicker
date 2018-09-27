63fa6f2c1814ae05fadb5c23cf882b56b0548c14
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-09-27 12:56:03 -0700

    Updates app.js comments.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// Listen for clicks on cat picture
+// Let's make this cat clickable!
 
 let cat_elem = document.getElementById('catPicture');
 let clicks = 0;
 
+// Listen for clicks on cat picture
 cat_elem.addEventListener('click', function() {
     clicks += 1;
     updateClicks();
-    console.log('Clicks: `${clicks}`');
 });
 
 function updateClicks() {
