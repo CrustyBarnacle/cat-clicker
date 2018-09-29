f931a1f4f0f73e7d64338943a2bb7abf0401741b
CrustyBarnacle <CrustyBarnaclee@users.noreply.github.com>
2018-09-28 22:34:08 -0700

    Updated to support two clickable cats and counts.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 // Let's make this cat clickable!
 
-let cat_elem = document.getElementById('catPicture0');
-let clicks = 0;
+let cat0 = document.getElementById('catPicture0');
+let cat1 = document.getElementById('catPicture1');
+let clicks0 = clicks1 = 0;
+
+const cats = [cat0, cat1];
 
 // Listen for clicks on cat picture
-cat_elem.addEventListener('click', function() {
-    clicks += 1;
+cat0.addEventListener('click', function() {
+    clicks0 += 1;
     updateClicks();
 });
 
+cat1.addEventListener('click', function() {
+    clicks1 += 1;
+    updateClicks1();
+});
+
 function updateClicks() {
-    const catClicks = document.getElementById('catClicks');
-    catClicks.innerHTML = `Total Clicks: ${clicks}`;
-}
+    const catClicks = document.getElementById('catClicks0');
+    catClicks.innerHTML = `Cat 1 Clicks: ${clicks0}`;
+};
+
+function updateClicks1() {
+    const catClicks = document.getElementById('catClicks1');
+    catClicks.innerHTML = `Cat 2 Clicks: ${clicks1}`;
+};
